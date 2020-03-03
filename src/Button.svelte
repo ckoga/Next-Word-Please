9420413cc6a307a9b0d1017bb0094021bad7444c
Colin Turing <colin.koga@gmail.com>
2020-03-03 07:41:01 -0700

    Add files and stlyes for basic layout

---
+++ src/Button.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let data
+</script>
+
+<button class='synonyms' type="button">{data}</button>
+
+<style>
+  .synonyms {
+	border-radius: 50px;
+	background-color: #ff3e00;
+	color: white;
+	cursor: pointer;
+}
+</style>
---
+++ src/Form.svelte
@@ -0,0 +1,28 @@
+<form>
+  <input type="text">
+  <button class="submit-btn" type="button">Submit</button>
+</form>
+
+
+<style>
+  form {
+	display: flex;
+	margin: 0 auto;
+	justify-content: center;
+	flex-direction: column;
+	width: 400px;
+}
+
+.submit-btn {
+	border-radius: 50px;
+	width: 175px;
+	margin: 0 auto;
+	background-color: #ff3e00;
+	color: white;
+	cursor: pointer;
+}
+
+.submit-btn:hover {
+	border: 1px solid red
+}
+</style>
